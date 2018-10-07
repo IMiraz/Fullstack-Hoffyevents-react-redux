c3de194b5bc326222068b2afa1c4649ce1850d8d
wasifmiraz <wasifmiraz@mail.com>
2018-10-07 12:48:51 +0600

    eventForm show and hide handler set

--- src/feature/event/eventDashboard/index.jsx
+++ src/feature/event/eventDashboard/index.jsx
@@ -3,7 +3,7 @@ import { Grid, GridColumn, Button } from 'semantic-ui-react';
 import EventList from '../eventList'
 import EventForm from '../eventForm'
 
-const events = [
+const eventsData = [
   {
     id: '1',
     title: 'Trip to Tower of London',
@@ -28,8 +28,58 @@ const events = [
       }
     ]
   },
+
   {
     id: '2',
+    title: 'Trip to Tower of London',
+    date: '2018-03-27T11:00:00+00:00',
+    category: 'culture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: "Tower of London, St Katharine's & Wapping, London",
+    hostedBy: 'Bob',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+      }
+    ]
+  },
+
+  {
+    id: '3',
+    title: 'Trip to Tower of London',
+    date: '2018-03-27T11:00:00+00:00',
+    category: 'culture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: "Tower of London, St Katharine's & Wapping, London",
+    hostedBy: 'Bob',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+      }
+    ]
+  },
+  {
+    id: '4',
     title: 'Trip to Punch and Judy Pub',
     date: '2018-03-28T14:00:00+00:00',
     category: 'drinks',
@@ -55,18 +105,40 @@ const events = [
 ]
 
 class EventDashboard extends Component {
-  render() {
+
+  state = {
+    events:eventsData,
+    isFormOpen:false
+
+  }
+
+handlerFormOpen = () => {
+  this.setState({
+     isFormOpen:true
+  })
+}
+
+  handlerFormCancle = () => {
+    this.setState({
+       isFormOpen:false
+    })
+
+
+}
+  render()
+  {
+
     return (
         <Grid>
            <GridColumn width={10}>
-           <EventList events={events} />
+           <EventList events={this.state.events} />
            </GridColumn>
            <GridColumn width={6}>
-           <Button positive content='Create Event'/>
-          <EventForm/>
+           <Button positive  onClick={this.handlerFormOpen} content='Create Event'/>
+          {this.state.isFormOpen &&
+            <EventForm  handlerFormCancle={this.handlerFormCancle}/>
+          }
            </GridColumn>
-
-
         </Grid>
     )
   }
