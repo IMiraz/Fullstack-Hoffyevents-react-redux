18bbe17f1c802e7f92daa8d564d2c8ee66e7a54f
wasifmiraz <wasifmiraz@mail.com>
2018-10-08 17:16:38 +0600

    Routes and navlink added in settings

--- src/feature/user/Settings/settingsDashboard/index.jsx
+++ src/feature/user/Settings/settingsDashboard/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import {GridColumn, Grid} from 'semantic-ui-react'
+import {Switch, Route} from 'react-router-dom'
 import SettingsNav from '../settingsNav'
+import AccountPage from '../accountPage'
+import PhotoPage from '../photoPage'
+import AboutPage from '../aboutPage'
+import BasicPage from '../basicPage'
 
 const SettingsDasboard = () => {
   return (
     <div>
     <Grid>
     <GridColumn width={12}>
- <h1>Settings </h1>
+    <Switch>
+      <Route path='/settings/basics' component={BasicPage}/>
+      <Route path='/settings/about' component={AboutPage}/>
+      <Route path='/settings/photos' component={PhotoPage}/>
+      <Route path='/settings/account' component={AccountPage}/>
+    </Switch>
     </GridColumn>
     <GridColumn width={4}>
     <SettingsNav/>
