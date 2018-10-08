b6bf81617b03a52dd08f8de6f20c9745d1a9fbdf
wasifmiraz <wasifmiraz@mail.com>
2018-10-08 11:06:48 +0600

    created some stateless component

---
+++ src/feature/event/eventDetailed/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EventDetailed = () => {
+  return (
+    <div>
+    <h1>EventDetailed</h1>
+
+    </div>
+  )
+}
+
+export default EventDetailed
---
+++ src/feature/user/Settings/settingsDashboard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SettingsDasboard = () => {
+  return (
+    <div>
+    <h1>SettingsDasboard</h1>
+
+    </div>
+  )
+}
+
+export default SettingsDasboard
