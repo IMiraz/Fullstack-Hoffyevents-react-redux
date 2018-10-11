863aa13ec2530340662c017dfa5e845c592c8ae7
imiraz <mdmirajulkarim@gmail.com>
2018-10-11 19:04:39 +0600

    created test redux actions

--- src/reducers/testReducer/index.js
+++ src/reducers/testReducer/index.js
@@ -1,11 +1,19 @@
-
+import {INCREMENT_COUNTER, DECREMENT_COUNTERs, DECREMENT_COUNTER} from '../../../src/feature/testArea/TestComponent/testActionFileds'
 const initialState = {
      data:526
 }
 
 const testReducer = (state = initialState, action) => {
 
-    return state
+    switch(action.type) {
+        case INCREMENT_COUNTER:
+        return {...state, data:state.data +1};
+
+        case DECREMENT_COUNTER:{
+             return {...state, data:state.data -1}
+        }
+        
+    }
 }
 
 export default testReducer
