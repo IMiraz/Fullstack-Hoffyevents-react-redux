6a42ee26c48315baa2ad5b574c1273debeca6e0c
imiraz <mdmirajulkarim@gmail.com>
2018-10-11 16:42:31 +0600

    setup reducers

---
+++ src/reducers/testReducer/index.js
@@ -0,0 +1,11 @@
+
+const initialState = {
+     date:42
+}
+
+const testReducer = (state = initialState, action) => {
+
+    return state
+}
+
+export default testReducer
--- src/store/ConfigureStore/index.js
+++ src/store/ConfigureStore/index.js
@@ -1,4 +1,6 @@
 import {createStore, applyMiddleware, compose} from 'redux'
+import rootReducer from '../../reducers/rootReducer'
+
 export const  ConfigureStore = (preloadState) => {
     const middleWares =[];
     const middleWareEnhancer = applyMiddleware(..middleWares);
