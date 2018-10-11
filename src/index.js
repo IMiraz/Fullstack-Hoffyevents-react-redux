1bbb41d003a337a25b49ff4e8f32f78625f77574
imiraz <mdmirajulkarim@gmail.com>
2018-10-11 16:48:32 +0600

    tell the our app about redux store, means index file setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './layout/App';
 import * as serviceWorker from './serviceWorker';
+import {ConfigureStore}  from '../src/store/ConfigureStore'
 
+
+const store= ConfigureStore();
 
 const rootElement = document.getElementById('root')
 
 let  render = () => {
     ReactDOM.render(
-<BrowserRouter><App /></BrowserRouter>,rootElement);
+        <Provider store={store}>
+        <BrowserRouter>
+       <App />
+      </BrowserRouter>
+        </Provider>
+,rootElement);
 }
 
 if(module.hot) {
--- src/store/ConfigureStore/index.js
+++ src/store/ConfigureStore/index.js
@@ -18,7 +18,4 @@ export const  ConfigureStore = (preloadState) => {
 
     return store
 
-
-
-
 }
