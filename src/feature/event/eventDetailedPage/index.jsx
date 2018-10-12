4c0abab268c1571ae5b9b122383e7cbbf150eb26
imiraz <mdmirajulkarim@gmail.com>
2018-10-12 21:11:31 +0600

    test: make eventdetailedHeder dynamic

--- src/feature/event/eventDetailedPage/index.jsx
+++ src/feature/event/eventDetailedPage/index.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import {Grid, GridColumn} from 'semantic-ui-react'
 
 import EventDetailedHeader from './eventDetailedHeader'
-import EventDetailedInfos from './eventDetailedInfo'
+import EventDetailedInfo from './eventDetailedInfo'
 import EventDetailedChat from './eventDetailedChat'
 import EventDetailedSidebar  from './eventDetailedSidebar'
+
+const event=
+  {
+    id: '1',
+    title: 'Trip to Tower of London',
+    date: '2018-03-27',
+    category: 'culture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: "Tower of London, St Katharine's & Wapping, London",
+    hostedBy: 'Bob',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+      }
+    ]
+  }
+
 
 
 const EventDetailedPage = () => {
@@ -12,9 +39,9 @@ const EventDetailedPage = () => {
     <div>
     <Grid>
       <GridColumn width={10}>
-      <EventDetailedHeader/>
-      <EventDetailedInfos/>
-      <EventDetailedChat/>
+      <EventDetailedHeader event={event}/>
+      <EventDetailedInfo event={event}/>
+      <EventDetailedChat event={event}/>
     
       </GridColumn>
       <GridColumn width={6}>
