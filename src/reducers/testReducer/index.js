cfbafa74f859980d08bddef973ca17c05bdbf248
imiraz <mdmirajulkarim@gmail.com>
2018-10-12 17:44:59 +0600

    create reducer util file :tada:

--- src/reducers/testReducer/index.js
+++ src/reducers/testReducer/index.js
@@ -1,7 +1,12 @@
-import {INCREMENT_COUNTER, DECREMENT_COUNTERs, DECREMENT_COUNTER} from '../../../src/feature/testArea/TestComponent/testActionFileds'
+import {createReducer} from '../../../src/common/util/reducerUtil'
+
+import {INCREMENT_COUNTER, DECREMENT_COUNTER} from '../../../src/feature/testArea/TestComponent/testActionFileds'
 const initialState = {
      data:526
 }
+
+
+
 
 const testReducer = (state = initialState, action) => {
 
