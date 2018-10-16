670e5c8a6f18b57ed0b797539220921e91754227
imiraz <mdmirajulkarim@gmail.com>
2018-10-16 12:36:22 +0600

    fix test auth modal closing with dispatch redux thunk

--- src/feature/auth/authActionsCreator/index.jsx
+++ src/feature/auth/authActionsCreator/index.jsx
@@ -1,14 +1,18 @@
 import {LOGIN_USER, LOGOUT_USER} from '../authActionsTypes'
+import {closeModal} from '../../Modal/modalActCreator'
+
 
 
 export const  login = (credentials) => {
-     return {
-          type:LOGIN_USER,
-          payload:{
-            credentials
-          }
+     return dispatch => {
+       dispatch({type:LOGIN_USER, payload:{
+          credentials
+       }})
+
+       dispatch(closeModal())
      }
-}
+     }
+
 
 export const logout = () => {
      return {
