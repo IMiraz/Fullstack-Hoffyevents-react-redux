8bed8ab4163a40eec7eb0258f3d0508e7a6a525f
imiraz <mdmirajulkarim@gmail.com>
2018-10-16 12:05:48 +0600

    test: creating actionscreator Async way

--- src/feature/testArea/TestComponent/testactionCreattors/index.jsx
+++ src/feature/testArea/TestComponent/testactionCreattors/index.jsx
@@ -1,5 +1,11 @@
 
-import {INCREMENT_COUNTER, DECREMENT_COUNTER} from '../testActionFileds'
+import {
+    INCREMENT_COUNTER,
+     DECREMENT_COUNTER,
+     COUNTER_ACTION_START,
+     COUNTER_ACTION_FINISHED
+    } from '../testActionFileds'
+import { resolve } from 'dns';
 
 
 export const incrementCounter = () => {
@@ -13,4 +19,39 @@ export const decrementCounter = () => {
      return {
           type:DECREMENT_COUNTER
      }
+}
+
+export const counterActionStart = () => {
+     return {
+         type:COUNTER_ACTION_START
+     }
+}
+
+export const counterActionFinished = () => {
+     return {
+         type:COUNTER_ACTION_FINISHED
+     }
+}
+
+const delay = (ms) => {
+     return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export const incrementAsync = () => {
+     return async dispatch => {
+         dispatch(counterActionStart())
+         await delay(1000);
+         dispatch({type:INCREMENT_COUNTER})
+         dispatch(counterActionFinished())
+     }
+}
+
+
+export const decrementAsync = () => {
+    return async dispatch => {
+        dispatch(counterActionStart())
+        await delay(1000);
+        dispatch({type:DECREMENT_COUNTER})
+        dispatch(counterActionFinished())
+    }
 }
