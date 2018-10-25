b6b5a030bb30005620ece465363c217b3760cc6b
imiraz <mdmirajulkarim@gmail.com>
2018-10-25 18:04:55 +0600

    fix user profile link for showing userinfo  have a dynamic data showing

--- src/feature/user/userDetailed/index.jsx
+++ src/feature/user/userDetailed/index.jsx
@@ -1,32 +1,39 @@
 import React, {Component} from 'react';
 import {Button, Card, Grid, Header, Icon, Image, Item, List, Menu, Segment} from "semantic-ui-react";
 import {connect} from 'react-redux'
-import {firestoreConnect} from 'react-redux-firebase'
+import {firestoreConnect, isEmpty} from 'react-redux-firebase'
 import {compose} from 'redux'
 import  UserdetailedHeader from './userDetailedHeader'
 import UserDetailedDescription from './userDetailedDescription'
 import UserDetailedSideBar from './userdetailedSidebar'
 import UserDetailedPhotos from './userDetailedPhotos'
 import UserDetailedEvent from './userDetailedEvent'
+import {UserDetailedQuery} from '../userQuery'
 
 
-const query=({auth}) => {
-    return [
-          {
-              collection:'users',
-              doc:auth.uid,
-              subcollections:[{collection:'photos'}],
-              storeAs:'photos'
-          }
-    ]
+
+
+const  mapState =(state, ownProps)=> {
+    let userUid = null;
+    let profile= {};
+
+            if(ownProps.match.params.id === state.auth.uid)
+            {
+            profile = state.firebase.profile 
+            }
+            else {
+                profile = !isEmpty(state.firestore.ordered.profile) && state.firestore.ordered.profile[0]
+                userUid = ownProps.match.params.id;
+            }
+
+     return {
+        profile,
+        userUid, 
+        auth:state.firebase.auth,
+        photos:state.firestore.ordered.photos
+     }
+   
 }
-
-
-const  mapState = state => ({
-    profile:state.firebase.profile,
-    auth:state.firebase.auth,
-    photos:state.firestore.ordered.photos
-})
 
 class UserDetailedPage extends Component {
 
@@ -48,5 +55,5 @@ class UserDetailedPage extends Component {
 
 export default compose(
     connect(mapState),
-  firestoreConnect(auth =>query(auth))
+  firestoreConnect((auth, userUid )=> UserDetailedQuery(auth, userUid))
 )  (UserDetailedPage);
