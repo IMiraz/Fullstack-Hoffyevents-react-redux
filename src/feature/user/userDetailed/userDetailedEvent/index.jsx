3df65906a090501a474975d8ed105a578bdcf10c
imiraz <mdmirajulkarim@gmail.com>
2018-11-06 20:49:35 +0600

    user prof eventshow #3 uses nice simple Tab

--- src/feature/user/userDetailed/userDetailedEvent/index.jsx
+++ src/feature/user/userDetailed/userDetailedEvent/index.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import{Card, Menu, Segment, Grid, Image,Header} from 'semantic-ui-react'
+import{Card, Menu, Segment, Grid, Image,Header, Tab} from 'semantic-ui-react'
 import format from 'date-fns/format'
+
+const panes = [
+    {menuItem:'All Events', panes:{key:'allEvents'}},
+    {menuItem:'Past Events', panes:{key:'pastEvents'}},
+    {menuItem:'Fututer Events', panes:{key:'futureEvents'}},
+    {menuItem:'Hosting', panes:{key:'hosted'}},
+]
+
 
 const  UserDetailedEvent = ({events, eventLoading}) => {
   return (
     <Grid.Column width={12}>
     <Segment attached loading={eventLoading}>
         <Header icon='calendar' content='Events'/>
-        <Menu secondary pointing>
-            <Menu.Item name='All Events' active/>
-            <Menu.Item name='Past Events'/>
-            <Menu.Item name='Future Events'/>
-            <Menu.Item name='Events Hosted'/>
-        </Menu>
+       <Tab panes={panes} menu={{secondary:true, pointing:true}}/>
 
         <Card.Group itemsPerRow={5}>
-        
   
       {events && events.map(event => (
           <Card key={event.id}>
@@ -25,8 +27,8 @@ const  UserDetailedEvent = ({events, eventLoading}) => {
                   {event.title}
               </Card.Header>
               <Card.Meta textAlign='center'>
-              <div>{format(event.date.toDate(), 'DD MMM YYYY')}</div>
-              <div>{format(event.date.toDate(), 'h:mm A')}</div>
+              <div>{format(event.date && event.date.toDate(), 'DD MMM YYYY')}</div>
+              <div>{format(event.date && event.date.toDate(), 'h:mm A')}</div>
               </Card.Meta>
           </Card.Content>
       </Card>
