f16fe4a734abd55b0cfdec9a72be35312dafe7f7
imiraz <mdmirajulkarim@gmail.com>
2018-11-09 19:36:03 +0600

    cloud functions working fine for follow and following options #2

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Here we work for cloud fucntion!");
- console.log('hello')
+exports.userfollowing = functions.firestore
+.document('users/{followerUid}/following/{followingUid}')
+.onCreate((event, contex) => {
+    console.log('v1');
+    const followerUid= contex.params.followerUid;
+    const followingUid = contex.params.followingUid;
+    const followerDoc = admin
+    .firestore()
+    .collection('users')
+    .doc(followerUid)
+    console.log(followerDoc);
+
+
+    return followerDoc.get().then(doc => {
+        let userData = doc.data();
+        console.log({ userData});
+
+        let follower = {
+            displayName: userData.displayName,
+            photoURL: userData.photoURL || '/assets/user.png',
+            city: userData.city || 'Unknown City'
+        };
+        return admin.firestore()
+        .collection('users')
+        .doc(followingUid)
+        .collection('followers')
+        .doc(followingUid)
+        .set(follower);
+
+    });
 });
